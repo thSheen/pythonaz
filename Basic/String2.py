dc150adabd93fe8d8b6bd1532d13b3093fce4335
thSheen <41039898+thSheen@users.noreply.github.com>
2018-07-26 14:49:36 -0300

    More about operate strings

---
+++ Basic/String2.py
@@ -0,0 +1,14 @@
+string1 = "She's "
+string2 = "probably "
+print(string1 + string2)
+print("She's " + "probably " + "crying")
+print("She's probably crying")
+print("Hi " * 5)
+print("Hi " * (5+4))
+# True or false control
+today = "thursday"
+print("day" in today)
+print("thurs" in today)
+print("fri" in today)
+print("champion" in "chewbacca")
+print("champ" in "champion")
