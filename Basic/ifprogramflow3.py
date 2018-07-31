f1ef04cfdd2186e35142cfa2135d2f66da9d82bd
thSheen <41039898+thSheen@users.noreply.github.com>
2018-07-30 22:33:29 -0300

    Part3 if program flow

---
+++ Basic/ifprogramflow3.py
@@ -0,0 +1,13 @@
+age = int(input("How old are you?"))
+
+# if (age >= 16) and (age <= 69):
+#     print("Have a good day at work!")
+# else:
+#     print("Enjoy your free time !")
+
+if (age < 16) or (age > 65):
+    print("Enjoy your free time!")
+else:
+    print("Have a good day at work!")
+# and = if just one is false wouldn't work.
+# or = check both, if one is true would work.
