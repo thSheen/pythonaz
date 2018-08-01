9de97cc7adf999953fc7d9d25cad04cad28d366b
thSheen <41039898+thSheen@users.noreply.github.com>
2018-07-31 21:38:42 -0300

    For loops
    number/name/table

---
+++ ProgramFlowControl/forloops3.py
@@ -0,0 +1,11 @@
+for state in ("not pinin'", "no more", "a stiff", "bereft of life"):
+    print("The parrot is " + state)
+# print("This parrot is ".format(state)
+
+for i in range(0, 100, 5):
+    print("i is now {}".format(i))
+
+for i in range(1, 13):
+    for j in range(1, 13):
+        print("{1} times {0} is {2}".format(i, j, i*j))
+    print("===============")
